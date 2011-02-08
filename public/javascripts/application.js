4de8753ebf00b0de2a910266f92d29bc631a9223
Михаил Костенчук <MichaelKostekchuk@MacBookMK-7.local>
2011-02-09 00:56:24 +0300

    before qaa12

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function over_student(){
+  $('<div id=\"student_#{student.id}_login\" class=\"student_login\">#{student.login}</div>').
+    appendTo('#student_#{student.id}');
+}
+function out_student(){
+  $('#student_#{student.id}_login').remove();
+}

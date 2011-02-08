926726233488e7006f70bcce5fb6fafe741ffa68
Юдин Алексей <qaa12@gmail.com>
2011-02-09 02:53:46 +0300

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Vekwak::Application.routes.draw do
   resources :sections
   resources :headman_auls, :only => [:index, :new, :create]
   resources :messages, :only => [:index, :new, :create]
-  match 'messages/show/:message_id' => 'messages/show', :as => :show
+  match 'messages/show/:message_id' => 'messages#show', :as => :show
   resources :news, :only => [:index, :new, :create]
   match 'days/new/:date(.:format)' => 'days#new', :as => :new_day
   match 'days/add/:id/:date(.:format)' => 'days#add', :as => :add_day

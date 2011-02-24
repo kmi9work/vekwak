d42ad952e5fdc7bb608c13e2aa8f7ed66562c3cd
Юдин Алексей <qaa12@gmail.com>
2011-02-25 00:16:12 +0300

    pravki

--- app/models/student.rb
+++ app/models/student.rb
@@ -26,14 +26,13 @@ class Student < ActiveRecord::Base
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :name, :password, :password_confirmation, :group, :last_name, :second_name, :karma, :rating, :avatar
 
-  has_many :posts
+  has_many :posts, :dependent => :delete_all
   has_many :messages, :dependent => :delete_all
-  has_many :messages_sent, :class_name => 'Message'
   has_many :post_rating_students, :dependent => :delete_all
   has_many :comment_rating_students, :dependent => :delete_all
-  has_many :days
-  has_many :comments
-  has_many :news  
+  has_many :days, :dependent => :delete_all
+  has_many :comments, :dependent => :delete_all
+  has_many :news, :dependent => :delete_all  
   has_attached_file :avatar, 
                     :styles => SIZES,
                     :url => "/system/images/:class/:attachment/:id/:style/:basename.:extension",

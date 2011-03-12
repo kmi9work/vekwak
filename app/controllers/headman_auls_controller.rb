e656105e0e5e8fe2fba7de8aa59781f004b5b96e
Михаил Костенчук <MichaelKostekchuk@iMac-iMac.local>
2011-03-12 23:19:05 +0300

    headman

--- app/controllers/headman_auls_controller.rb
+++ app/controllers/headman_auls_controller.rb
@@ -8,19 +8,23 @@ class HeadmanAulsController < ApplicationController
   end
   
   def create
-    @headman_aul = HeadmanAul.create(params[:headman_aul])
-    success = @headman_aul && @headman_aul.save
-    info_msg
-    if success and @headman_aul.errors.empty?
-      respond_to do |format|
-        format.html {redirect_back_or_default('/')}
-        format.js
+    if @student and @student.headman
+      @headman_aul = HeadmanAul.create(params[:headman_aul])
+      success = @headman_aul && @headman_aul.save
+      info_msg
+      if success and @headman_aul.errors.empty?
+        respond_to do |format|
+          format.html {redirect_back_or_default('/')}
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html {render :action => 'new'}
+          format.js {render 'fail_create.js.erb'}
+        end  
       end
     else
-      respond_to do |format|
-        format.html {render :action => 'new'}
-        format.js {render 'fail_create.js.erb'}
-      end  
+      flash[:error] = "You are not a Headman!"
     end
   end
 end
